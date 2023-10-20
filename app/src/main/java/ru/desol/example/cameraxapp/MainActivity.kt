b1c146c1cf2b22bbec90672c2999475b8807052e
Denis Solovyov <infinityx93@gmail.com>
2023-10-20 11:37:20 +0300

    8. (Optional) Combine VideoCapture with other use cases

--- app/src/main/java/ru/desol/example/cameraxapp/MainActivity.kt
+++ app/src/main/java/ru/desol/example/cameraxapp/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.FallbackStrategy
 import androidx.camera.video.MediaStoreOutputOptions
 import androidx.camera.video.Quality
 import androidx.camera.video.QualitySelector
@@ -223,12 +224,16 @@ class MainActivity : AppCompatActivity() {
                 }
 
             val recorder = Recorder.Builder()
-                .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
-                .build()
+                .setQualitySelector(
+                    QualitySelector.from(
+                        Quality.HIGHEST,
+                        FallbackStrategy.higherQualityOrLowerThan(Quality.SD)
+                    )
+                ).build()
             videoCapture = VideoCapture.withOutput(recorder)
 
 
-//            imageCapture = ImageCapture.Builder().build()
+            imageCapture = ImageCapture.Builder().build()
 
 //            val imageAnalyzer = ImageAnalysis.Builder()
 //                .build()
@@ -246,7 +251,9 @@ class MainActivity : AppCompatActivity() {
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture)
+                cameraProvider
+                    .bindToLifecycle(this, cameraSelector, preview, imageCapture, videoCapture)
+
 
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
